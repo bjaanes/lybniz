46576eff8b53c90403a17c25f9c1c26bb13aa9e6
tygier <tygier@09313a66-9118-0410-9561-80d697c507e0>
2006-07-18 21:30:09 +0000

    setup.py added
    git-svn-id: https://lybniz2.svn.sourceforge.net/svnroot/lybniz2/trunk@5 09313a66-9118-0410-9561-80d697c507e0

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+data_files = [('share/applications',['lybniz.desktop'])]
+
+setup(
+        name = 'lybniz',
+        version = '1.0.1',
+        description = 'Graph Plotter',
+        author = 'Thomas Führinger, Sam Tygier',
+        author_email = 'ThomasFuhringer@Yahoo.com, samtygier@yahoo.co.uk',
+	contact = 'Sam Tygier',
+	contact_email = 'samtygier@yahoo.co.uk',
+        url = 'http://lybniz2.sourceforge.net/',
+        scripts = ['lybniz.py'],
+        data_files = data_files,
+       license = 'BSD',
+        )
+
